ca234f6dcb6cf783b7d802d611381508414af83e
wjj <493305086@qq.com>
2023-12-30 11:15:14 +0800

    新增了一个User实体类

---
+++ src/main/java/com/example/springboot3_04_druid/domain/User.java
@@ -0,0 +1,10 @@
+package com.example.springboot3_04_druid.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String sex;
+    private Integer age;
+}
