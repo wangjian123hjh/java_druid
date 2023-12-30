5bbc85693fdcb480b94770e0193be2df68406196
wjj <493305086@qq.com>
2023-12-30 11:03:39 +0800

    edit BookServiceImpl

--- src/main/java/com/example/springboot3_04_druid/dao/impl/BookServiceImpl.java
+++ src/main/java/com/example/springboot3_04_druid/dao/impl/BookServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class BookServiceImpl {
     public void save(){
-
+        System.out.println("你好");
+        System.out.println("第一次使用git来进行推送");
     }
 }
