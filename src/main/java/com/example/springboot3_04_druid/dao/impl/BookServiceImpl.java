985064228d520ab6a285d41eca1fdbbf7ae6c5f3
wjj <493305086@qq.com>
2023-12-30 10:50:45 +0800

    新增了一个BookServiceImpl类

---
+++ src/main/java/com/example/springboot3_04_druid/dao/impl/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.springboot3_04_druid.dao.impl;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookServiceImpl {
+    public void save(){
+
+    }
+}
